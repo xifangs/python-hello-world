9a05d40e43da66255e97e7fe24b93741b44141c8
xifangs <32235167+xifangs@users.noreply.github.com>
2017-09-24 15:32:51 +0800

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+#message="世界第一名"
+#print (message)
+#message="fdfsfs"
+#print(message)
+#name=["wuxifang","liudehua","liangchaogwei"]
+#for x in name:
+#	print(x)
+
+#pingfang=[value**2 for value in xrange(1,10)]
+#print(pingfang)
+
+for i in range(1,10):
+	for j in range(1,i+1):
+		print(str(i) + "*" + str(j) + "=" + str(j*i))
+
+	print("")
